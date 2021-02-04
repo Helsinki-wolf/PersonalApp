0c9de533e4d6045a70f1331a713ee57b574c4a8e
hackerrank <me@hackerrank.com>
2021-02-04 18:29:34 +0700

    initial

---
+++ app.js
@@ -0,0 +1,20 @@
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+const cors = require('cors')
+require('dotenv').config()
+
+app.use(express.urlencoded({extended: true}))
+app.use(cors())
+app.use(express.json())
+
+
+// const {router} = require('./routes')
+
+// app.use('/', router)
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
