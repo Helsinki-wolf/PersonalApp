686b86ae79eef2c74581576c397afccb7fdd00e8
hackerrank <me@hackerrank.com>
2021-02-05 00:59:36 +0700

    little fix

--- controllers/ControllerHarvardArt.js
+++ controllers/ControllerHarvardArt.js
@@ -2,8 +2,6 @@ const axios = require('axios')
 
 class ControllerHarvardArt {
     static read(req, res, next){
-        console.log('harvard')
-        console.log(process.env.HARVARD_API_KEY)
         axios({
             method: 'get',
             url: `https://api.harvardartmuseums.org/object?apikey=${process.env.HARVARD_API_KEY}&page=${req.params.page}&size=5`,
