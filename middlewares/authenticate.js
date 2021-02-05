f3583420bb4db2d79b9f03b6165f7c765dd52927
hackerrank <me@hackerrank.com>
2021-02-05 11:42:48 +0700

    load zomato by indo

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -3,6 +3,7 @@ const jwt = require("jsonwebtoken")
 const authenticate = (req, res, next) => {
   try {
     const token = req.headers.token
+    console.log(token)
     const decoded = jwt.verify(token, process.env.SECRET_KEY)
     req.decoded = decoded
     next()
