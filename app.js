30d24edfa6ffd5ebb2a1ffa29160b1fffc49210f
hackerrank <me@hackerrank.com>
2021-02-05 09:46:03 +0700

    test deploy

--- app.js
+++ app.js
@@ -4,12 +4,19 @@ const app = express()
 const port = 3000
 const router = require('./routes')
 const cors = require('cors')
+const path=require('path')
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cors())
 
 app.use(router)
+
+
+app.use(express.static(path.join(__dirname, '/client')));
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client', 'index.html'));
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
